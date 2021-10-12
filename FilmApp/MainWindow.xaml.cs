ae9d34342ba9af0a90854e15b10b69cc49b0669f
pavlo <pavlo@DESKTOP-SM098C1>
2021-10-12 15:19:02 +0300

    design of menu

--- FilmApp/MainWindow.xaml.cs
+++ FilmApp/MainWindow.xaml.cs
@@ -17,6 +17,7 @@ using System.Text.Json;
 using System.Text.Json.Serialization;
 using System.IO;
 using Newtonsoft.Json;
+using FilmApp.Model;
 
 namespace FilmApp
 {
@@ -31,145 +32,157 @@ namespace FilmApp
             InitializeComponent();
             list = new FilmList();
            
-            FilmTable.ItemsSource = list.List;
+            //FilmTable.ItemsSource = list.List;
         }
 
-        private void BFindTheLongesFilms_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                FilmTable.ItemsSource = list.FindForDirectorsLongestFilm();
-                FilmTable.Items.Refresh();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
+        //private void BFindTheLongesFilms_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        FilmTable.ItemsSource = list.FindForDirectorsLongestFilm();
+        //        FilmTable.Items.Refresh();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
 
-        private void BSort_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                list.SortByCountry();
-                FilmTable.ItemsSource = list.List;
-                FilmTable.Items.Refresh();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+        //private void BSort_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        list.SortByCountry();
+        //        FilmTable.ItemsSource = list.List;
+        //        FilmTable.Items.Refresh();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
             
-        }
+        //}
 
-        private void BWriteFilmsToFile_Click(object sender, RoutedEventArgs e)
-        {
-            string films = System.Text.Json.JsonSerializer.Serialize(list);
-            File.WriteAllText($"{Directory.GetCurrentDirectory()}/films.json",films);
-        }
+        //private void BWriteFilmsToFile_Click(object sender, RoutedEventArgs e)
+        //{
+        //    string films = System.Text.Json.JsonSerializer.Serialize(list);
+        //    File.WriteAllText($"{Directory.GetCurrentDirectory()}/films.json",films);
+        //}
 
-        private void BReadFilmsFromFile_Click(object sender, RoutedEventArgs e)
-        {
-            using (StreamReader read = new StreamReader($"{Directory.GetCurrentDirectory()}/films.json"))
-            {
-                string json = read.ReadToEnd();
-                list = JsonConvert.DeserializeObject<FilmList>(json);
-            }
-            FilmTable.ItemsSource = list.List;
-            FilmTable.Items.Refresh();
-        }
+        //private void BReadFilmsFromFile_Click(object sender, RoutedEventArgs e)
+        //{
+        //    using (StreamReader read = new StreamReader($"{Directory.GetCurrentDirectory()}/films.json"))
+        //    {
+        //        string json = read.ReadToEnd();
+        //        list = JsonConvert.DeserializeObject<FilmList>(json);
+        //    }
+        //    FilmTable.ItemsSource = list.List;
+        //    FilmTable.Items.Refresh();
+        //}
 
-        private void BFindPopularActor_Click(object sender, RoutedEventArgs e)
-        {
-            MessageBox.Show(list.FindPopularActor());
-        }
+        //private void BFindPopularActor_Click(object sender, RoutedEventArgs e)
+        //{
+        //    MessageBox.Show(list.FindPopularActor());
+        //}
 
-        private void BShowAllList_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                FilmTable.ItemsSource = list.List;
-                FilmTable.Items.Refresh();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
+        //private void BShowAllList_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        FilmTable.ItemsSource = list.List;
+        //        FilmTable.Items.Refresh();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
 
-        private void BFindFilmsWithActor_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                FilmTable.ItemsSource = list.FindAllFilmsWithActor(ActorName.Text);
-                FilmTable.Items.Refresh();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+        //private void BFindFilmsWithActor_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        FilmTable.ItemsSource = list.FindAllFilmsWithActor(ActorName.Text);
+        //        FilmTable.Items.Refresh();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
             
+        //}
+
+        //private void ActorName_GotFocus(object sender, RoutedEventArgs e)
+        //{
+        //    ActorName.Text = "";
+        //}
+
+        //private void ActorName_LostFocus(object sender, RoutedEventArgs e)
+        //{
+        //    if (ActorName.Text == "")
+        //        ActorName.Text = "Enter actor name";
+        //}
+
+        //private void Country_LostFocus(object sender, RoutedEventArgs e)
+        //{
+        //    if (Country.Text == "")
+        //        Country.Text = "Enter country";
+        //}
+
+        //private void Country_GotFocus(object sender, RoutedEventArgs e)
+        //{
+        //    Country.Text = "";
+        //}
+
+        //private void BFindFilmsByCountry_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        FilmTable.ItemsSource = list.FindByCountry(Country.Text);
+        //        FilmTable.Items.Refresh();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
+
+        //private void BFindOldestExpesive_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        FilmTable.ItemsSource = list.FindTheMostExpensiveAndOldest();
+        //        FilmTable.Items.Refresh();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
+
+        //private void BFindDirectorsLowestBudget_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        FilmTable.ItemsSource = list.FindSameDirectorsAndLowesBudget();
+        //        FilmTable.Items.Refresh();
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        MessageBox.Show(ex.Message);
+        //    }
+        //}
+
+        private void BOpenMenu_Click(object sender, RoutedEventArgs e)
+        {
+            BOpenMenu.Visibility = Visibility.Collapsed;
+            BCloseMenu.Visibility = Visibility.Visible;
         }
 
-        private void ActorName_GotFocus(object sender, RoutedEventArgs e)
+        private void BCloseMenu_Click(object sender, RoutedEventArgs e)
         {
-            ActorName.Text = "";
-        }
-
-        private void ActorName_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (ActorName.Text == "")
-                ActorName.Text = "Enter actor name";
-        }
-
-        private void Country_LostFocus(object sender, RoutedEventArgs e)
-        {
-            if (Country.Text == "")
-                Country.Text = "Enter country";
-        }
-
-        private void Country_GotFocus(object sender, RoutedEventArgs e)
-        {
-            Country.Text = "";
-        }
-
-        private void BFindFilmsByCountry_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                FilmTable.ItemsSource = list.FindByCountry(Country.Text);
-                FilmTable.Items.Refresh();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void BFindOldestExpesive_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                FilmTable.ItemsSource = list.FindTheMostExpensiveAndOldest();
-                FilmTable.Items.Refresh();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
-
-        private void BFindDirectorsLowestBudget_Click(object sender, RoutedEventArgs e)
-        {
-            try
-            {
-                FilmTable.ItemsSource = list.FindSameDirectorsAndLowesBudget();
-                FilmTable.Items.Refresh();
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
+            BOpenMenu.Visibility = Visibility.Visible;
+            BCloseMenu.Visibility = Visibility.Collapsed;
         }
     }
 }
