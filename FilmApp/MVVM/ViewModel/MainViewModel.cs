3b015031f12096c592e882e446e2d4c21a566fb4
pavlo <pavlo@DESKTOP-SM098C1>
2021-10-14 10:28:01 +0300

    connected view model to main view

--- FilmApp/MVVM/ViewModel/MainViewModel.cs
+++ FilmApp/MVVM/ViewModel/MainViewModel.cs
@@ -6,9 +6,20 @@ namespace FilmApp.MVVM.ViewModel
 {
     class MainViewModel: ObservableObject
     {
+        public DashboardViewModel Dashboard { get; set; }
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set {
+                _currentView = value;
+                OnPropertyChanged();
+            }
+        }
         public MainViewModel()
         {
-
+            Dashboard = new DashboardViewModel();
+            CurrentView = Dashboard;
         }
     }
 }
